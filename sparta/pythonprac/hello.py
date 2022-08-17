66a5cf0598fbe17198804cdef0d0316407ba6618
fatiger <fatiger92@gmail.com>
2022-08-17 14:11:50 +0900

    commit
    commit

---
+++ sparta/pythonprac/hello.py
@@ -0,0 +1,30 @@
+
+# 변수 선언에 자료형 안 적어도 됨
+# a = 2
+# b = 3
+# c = "Victor"
+# d = "Ji"
+# print(a+b, c+d);
+
+# 리스트 정의
+# a_list = ['사과', '배', '감']
+# a_list.append('수박')
+# print(a_list)
+
+# for 문과 딕셔너리
+# a_dict = {
+#     'name' : 'bob',
+#     'age': 27
+# }
+# a_dict['gender'] = "male"
+# for key in a_dict:
+#     print(key, ':', a_dict[key])
+
+# 함수
+# def sum(a,b):
+#     print('더하자')
+#     return a+b;
+#
+# result = sum(1,2)
+# print(result)
+
